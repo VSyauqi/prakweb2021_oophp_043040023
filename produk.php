a2e73afd5b0067a7aeec6ce999c96e23ddc54b14
VSyauqi <63843171+VSyauqi@users.noreply.github.com>
2021-09-23 18:57:50 +0700

    (video 3) latihan mengisi nilai kedalam property

--- produk.php
+++ produk.php
@@ -1,7 +1,7 @@
 <?php
 
 // jualan produk
-// film dan game
+// komik dan game
 
 class Produk
 {
@@ -11,10 +11,18 @@ class Produk
     $harga = 0;
 }
 
-$produk1 = new Produk();
-$produk1->judul = "Naruto";
-var_dump($produk1);
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
 
-$produk2 = new Produk();
-$produk2->judul = "Uncharted";
-var_dump($produk2->judul);
+// $produk2 = new Produk();
+// $produk2->judul = "Uncharted";
+// var_dump($produk2->judul);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen Jump";
+$produk3->harga = 30000;
+
+echo "Komik : produk3->penulis, $produk3->penerbit";
